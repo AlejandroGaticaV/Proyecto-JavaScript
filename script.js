eb804b12d12d30b7c46c0744e9f208b14fa67335
Alejandro <michi@gmail.com>
2025-05-19 18:16:05 -0400

    Style – estilos implementados en proyecto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const students = []
+const students = [];
 
 document.getElementById("studentform").addEventListener("submit", function (e) {
     e.preventDefault();
@@ -8,37 +8,38 @@ document.getElementById("studentform").addEventListener("submit", function (e) {
     const grade = parseFloat(document.getElementById("grade").value);
 
     if (grade > 7 || grade < 1 || !name || !lastName || isNaN(grade)) {
-        alert("Error al ingresar los datos")
-        return
+        alert("Error al ingresar los datos");
+        return;
     }
-    const student = { name, lastName, grade }
-    students.push(student)
-    console.log(students)
-    addStudentToTable(student)
+
+    const student = { name, lastName, grade };
+    students.push(student);
+    addStudentToTable(student);
     calcularPromedio();
     this.reset();
+});
 
-})
+const tableBody = document.getElementById("studentTableBody");
 
-const tableBody = document.querySelector("#studentTable tbody")
 function addStudentToTable(student) {
     const row = document.createElement("tr");
     row.innerHTML = `
     <td>${student.name}</td>
     <td>${student.lastName}</td>
     <td>${student.grade}</td>
-    `;
-    tableBody.appendChild(row)
+  `;
+    tableBody.appendChild(row);
 }
 
 const promDiv = document.getElementById("average");
+
 function calcularPromedio() {
     if (students.length === 0) {
-        promDiv.innerHTML = "Promedio General del Curso : N/A";
-        return
+        promDiv.innerHTML = "Promedio General del Curso: N/A";
+        return;
     }
 
     const total = students.reduce((acc, student) => acc + student.grade, 0);
     const average = total / students.length;
-    promDiv.innerHTML = `Promedio General del curso : ${average.toFixed(2)}`
+    promDiv.innerHTML = `Promedio General del Curso: ${average.toFixed(2)}`;
 }
